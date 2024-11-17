9030f935f60cd8dd714709ab849b9bf7f648ec64
Connell Boyce <connellboyce17@gmail.com>
2024-11-17 10:35:30 -0500

    feat: Update navbar and setup base resume page

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,15 +1,22 @@
 import Curtain from "@/app/components/curtain";
 import Navbar from "@/app/components/navbar";
 import Footer from "@/app/components/footer";
+import Head from "next/head";
+import React from "react";
+import ResumeDownloadButton from "@/app/components/resume/resume-download-button";
 
 export default function Resume() {
     return (
         <main className="flex flex-col bg-black container mx-auto font-sans scroll-smooth">
+            <Head>
+                <title>Resume - Connell Boyce</title>
+            </Head>
             <Curtain />
             <div className="min-h-screen">
                 <Navbar/>
                 <div className="mt-24">
-                    <p>Resume</p>
+                    <h5 className="text-4xl font-bold text-white my-2 text-center"><span className="text-[#f9004d]">my</span> resume</h5>
+                    <ResumeDownloadButton/>
                 </div>
             </div>
             <Footer/>
