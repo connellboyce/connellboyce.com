cc1d18d0684102eeaa7d390b6076a398a08f17a4
Connell Boyce <connellboyce17@gmail.com>
2024-11-22 10:10:30 -0500

    fiX: Escape apostrophe

--- app/components/resume/education.tsx
+++ app/components/resume/education.tsx
@@ -6,7 +6,7 @@ export default function Education() {
             <h3 className="text-3xl md:text-4xl font-bold text-white">my <span className="text-[#f9004d]">education</span></h3>
             <h5 className="text-2xl md:text-3xl mt-4">The Pennsylvania State University</h5>
             <div>
-                <p className="md:text-xl text-[#f9004d]">Bachelor's Degree of Science, Information Sciences and Technology</p>
+                <p className="md:text-xl text-[#f9004d]">Bachelor&apos;s Degree of Science, Information Sciences and Technology</p>
                 <p className="md:text-xl">Design and Development</p>
                 <p><i>Major GPA:  3.49</i></p>
             </div>
